3e9b8478bcbf788f5cbc7d1cd27a391509d7268c
fly2moon2 <fly2moon2@yahoo.com>
2022-05-08 19:59:15 +0800

    installed redux react-redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+import registerServiceWorker from './registerServiceWorker';
+
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
+import App from './App';
+import './index.css';
+
+const store = createStore(tasks);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
+
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +36,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
